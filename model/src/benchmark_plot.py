697af019d50294a1c3dd0edcb0f94e980ce1a46e
henry-AY <henryyost402@gmail.com>
2025-10-28 16:18:57 -0700

    notebook installation

--- model/src/benchmark_plot.py
+++ model/src/benchmark_plot.py
@@ -11,13 +11,13 @@ DEVICE = df_filtered['device'].iloc[0]
 
 df_filtered = df_filtered.drop(["model_name", "device", "round", "elapsed_time_sec", "input_length", "generated_length", "seed", "temperature", "top_p", "top_k", "response_preview"], axis=1)
 
-df_tps = df_filtered.drop(["round_avg_sec", "round_std_sec"], axis=1)
+df_tps = df_filtered.drop(["run", "round_avg_sec", "round_std_sec"], axis=1)
 df_sec = df_filtered.drop(["tokens_per_sec"], axis=1)
 
 print(df_tps)
 print(df_sec)
 
-plt.plot(df_tps["run"], df_tps["token_per_sec"])
+plt.plot(df_tps)
 plt.show()
 
 plt.plot(df_sec)
